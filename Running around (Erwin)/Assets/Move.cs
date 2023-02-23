3a06e559cbc18692df912e478cddf5ae55ec3e08
Safwan <35345@ma-web.nl>
2023-02-23 13:01:25 +0100

    Les3

---
+++ Running around (Erwin)/Assets/Move.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 50f;
+    [SerializeField] private float rotSpeed = 50f;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Time.deltaTime * speed * Input.GetAxis("Vertical");
+
+        float rot = Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
+        rb.transform.Rotate(new Vector3(0, rot, 0));
+        rb.velocity = rb.transform.forward * move;
+        
+    }
+
+
+}
+
+
+
